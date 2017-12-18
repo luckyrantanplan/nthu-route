136c44c97c6db8af2cf77d63798cd4428f9b678b
Florian Prud'homme <florian.prudhomme@gmail.com>
2017-12-18 22:11:37 +0100

    add spdlog

--- src/router/route.cpp
+++ src/router/route.cpp
@@ -11,26 +11,36 @@
 #include "Layerassignment.h"
 #include "parameter.h"
 
+#define SPDLOG_TRACE_ON
+#include <spdlog/spdlog.h>
+
 void dataPreparation(ParameterAnalyzer& ap, RoutingRegion& builder);
 
 int main(int argc, char* argv[]) {
-    std::cout << "=======================================================" << std::endl //
-            << "= NTHU-Route                                          =" << std::endl //
-            << "= Version 3.0 is developed by                         =" << std::endl //
-            << "= Yen-Jung Chang, Yu-ting Lee, Tsung-Hsien Lee        =" << std::endl //
-            << "= Jhih-Rong Gao, Pei-Ci Wu, Florian Prud'homme        =" << std::endl //
-            << "= Adviser: Ting-Chi Wang (tcwang@cs.nthu.edu.tw)      =" << std::endl //
-            << "= http://www.cs.nthu.edu.tw/~tcwang/nthuroute/        =" << std::endl //
-            << "=======================================================" << std::endl << std::endl //
-            << "=======================================================" << std::endl //
-            << "= Running FLUTE for initial Steiner tree              =" << std::endl //
-            << "= FLUTE is developed by Dr. Chris C. N. Chu           =" << std::endl //
-            << "=                       Iowa State University         =" << std::endl //
-            << "= http://home.eng.iastate.edu/~cnchu/                 =" << std::endl //
-            << "=======================================================" << std::endl //
-            << "= Using Boost Library                                 =" << std::endl //
-            << "= For internal data structure                         =" << std::endl //
-            << "=======================================================" << std::endl;
+
+    auto console_sp = spdlog::stdout_logger_mt("NTHUR");
+    spdlog::logger& log = *console_sp;
+    spdlog::set_level(spdlog::level::trace);
+    spdlog::set_pattern("%Y%m%d%H%M%S.%f] %L %v");
+    SPDLOG_TRACE(console_sp, "test the trace");
+
+    log.info("======================================================="); //
+    log.info("= NTHU-Route                                          =");
+    log.info("= Version 3.0 is developed by                         =");
+    log.info("= Yen-Jung Chang, Yu-ting Lee, Tsung-Hsien Lee        =");
+    log.info("= Jhih-Rong Gao, Pei-Ci Wu, Florian Prud'homme        =");
+    log.info("= Adviser: Ting-Chi Wang (tcwang@cs.nthu.edu.tw)      =");
+    log.info("= http://www.cs.nthu.edu.tw/~tcwang/nthuroute/        =");
+    log.info("=======================================================\n");
+    log.info("=======================================================");
+    log.info("= Running FLUTE for initial Steiner tree              =");
+    log.info("= FLUTE is developed by Dr. Chris C. N. Chu           =");
+    log.info("=                       Iowa State University         =");
+    log.info("= http://home.eng.iastate.edu/~cnchu/                 =");
+    log.info("=======================================================");
+    log.info("= Using Boost Library                                 =");
+    log.info("= For internal data structure                         =");
+    log.info("=======================================================\n");
 
     clock_t t0 = clock();
     ParameterAnalyzer ap(argc, argv);
@@ -42,7 +52,7 @@ int main(int argc, char* argv[]) {
     Congestion congestion(routingData.get_gridx(), routingData.get_gridy());
 
     clock_t t1 = clock();
-    Construct_2d_tree tree(ap.routing_param(), ap.parameter(), routingData,congestion);
+    Construct_2d_tree tree(ap.routing_param(), ap.parameter(), routingData, congestion);
     clock_t t2 = clock();
     // now the post processing is handle by Construct_2d_tree
     clock_t t3 = clock();
